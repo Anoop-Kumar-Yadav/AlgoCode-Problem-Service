b911ebed05e14a2da0e0dd53524dfda6bceb69ad
Anoop-Kumar-Yadav <165376785+Anoop-Kumar-Yadav@users.noreply.github.com>
2026-03-09 16:30:10 +0530

    Feat: implement status code for not implemented callbacks using http-status-code library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,3 +1,5 @@
+const { StatusCodes } = require('http-status-codes')
+
 // Executes on request : /api/v1/problems/ping
 function pingProblemControllerCheck(req, res) {
     return res.json({
@@ -6,23 +8,33 @@ function pingProblemControllerCheck(req, res) {
 }
 
 function addProblem(req, res) {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message : "NOT IMPLEMENTED"
+    })
 }
 
 function getProblem(req, res) {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message : "NOT IMPLEMENTED"
+    })    
 }
 
 function getProblems(req, res) {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message : "NOT IMPLEMENTED"
+    }) 
 }
 
 function deleteProblem(req, res) {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message : "NOT IMPLEMENTED"
+    })
 }
 
 function updateProblem(req, res) {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message : "NOT IMPLEMENTED"
+    })
 }
 
 module.exports = {
