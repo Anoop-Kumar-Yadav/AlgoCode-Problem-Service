58366c20d08fb83835aacf2f4299289f5879ec60
Anoop-Kumar-Yadav <165376785+Anoop-Kumar-Yadav@users.noreply.github.com>
2026-03-10 16:29:20 +0530

    Feat: Prepared Schema for problem

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const { PORT } = require('./config/server.config')
 
+const { PORT } = require('./config/server.config')
 const apiRouter = require('./routes/index')
 const errorHandler = require("./utils/errorHandler")
+const connectDB = require("./config/db.config")
 
 const app = express()
-
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -14,8 +14,8 @@ app.use(express.text())
 // If any request comes and route start with /api , we ,map it to apiRouter
 app.use("/api",apiRouter)
 
-
 app.use(errorHandler)
-app.listen(PORT , () => {
+app.listen(PORT , async () => {
     console.log(`SERVER started at : ${PORT}`);
+    await connectDB()
 })
